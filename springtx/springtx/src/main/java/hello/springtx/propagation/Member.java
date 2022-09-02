49b9cc46b9927f0315539997f909d7dc8b8fc2b2
WooJinDeve <jwj060111@gmail.com>
2022-09-02 12:57:41 +0900

    transaction

---
+++ springtx/springtx/src/main/java/hello/springtx/propagation/Member.java
@@ -0,0 +1,28 @@
+package hello.springtx.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
